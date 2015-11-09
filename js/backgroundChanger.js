a2735d96b05a3fcecdc673e2a698692754fa0632
MiskoG <mgiboreau@gmail.com>
2015-11-09 15:07:51 +0100

    Change logo home section
    rajout du logo accompagné de la devise

--- js/backgroundChanger.js
+++ js/backgroundChanger.js
@@ -5,29 +5,29 @@ $(window).load(function(){
     function changeBackground() {
 
         if(imgNumber==1) {
-            $('#tf-home').delay(6500).fadeTo(400, 0.4, function()
+            $('#tf-home').delay(7000).fadeTo(400, 0.4, function()
             {
                 $(this).css('background-image', 'url(img/home3.jpg)');
             }).fadeTo(300, 1);
             imgNumber++;
         }
         /*else {if(imgNumber==2) {
-            $('#tf-home').delay(6500).fadeTo(400, 0.4, function()
+            $('#tf-home').delay(7000).fadeTo(400, 0.4, function()
             {
                 $(this).css('background-image', 'url(img/home3.jpg)');
             }).fadeTo(400, 1);
             imgNumber++;
         }*/ else {
-            $('#tf-home').delay(6500).fadeTo(400, 0.4, function()
+            $('#tf-home').delay(7000).fadeTo(400, 0.4, function()
             {
                 $(this).css('background-image', 'url(img/home1.jpg)');
             }).fadeTo(300, 1);
             imgNumber=1;
         }/*}*/
 
-        setTimeout(changeBackground(),6500);
+        setTimeout(changeBackground(),7000);
     }
 
-    setTimeout(changeBackground(),6500);
+    setTimeout(changeBackground(),7000);
 
 });
